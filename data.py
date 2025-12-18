26c3843358660c3a821edaac701b6f4ed16c5405
Sarthak-Sharma03 <78611655+Sarthak-Sharma03@users.noreply.github.com>
2025-12-18 11:08:20 +0530

    Delete data.py

--- data.py
+++
@@ -1,7 +0,0 @@
-url = "https://chitkara-hackathon.s3.ap-south-1.amazonaws.com/Chitkara_Anomaly_Detection.zip"
-wget.download(url)
-
-from zipfile import ZipFile
-
-with ZipFile("C:\Users\lenovo\Downloads\Send", 'r') as zObject:
-    zObject.extractall(path="C:\Users\lenovo\Downloads\Send\data")
